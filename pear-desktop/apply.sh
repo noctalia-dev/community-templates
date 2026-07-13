9ff5f5a170388550e478854834157367f10b6fa8
jinra321 <jinra321@gmail.com>
2026-07-12 21:46:06 -0700

    added fle check fixed path

--- pear-desktop/apply.sh
+++ pear-desktop/apply.sh
@@ -3,4 +3,8 @@
 JSON_FILE="$XDG_CONFIG_HOME/YouTube Music/config.json"
 THEME_PATH="$XDG_CONFIG_HOME/YouTube Music/noctalia.css"
 
+if [ ! -f "$JSON_FILE" ]; then
+    exit 0
+fi
+
 jq --arg new_theme "$THEME_PATH" '.options.themes = [$new_theme]' "$JSON_FILE" > temp.json && mv temp.json "$JSON_FILE"
