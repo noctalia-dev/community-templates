90f4f89f38726bf2c21f7cb710112ede0df995e1
icy <143240188+bwoah-md@users.noreply.github.com>
2026-08-15 21:22:45 +0530

    Add check for colors.py modification time
    Check if colors.py is newer than the script before executing. This fixes the behavior of script execution whenever the user changes templates of any other gui/cli app through Noctalia. It'll only check if there has been any changes in colors.py and then exequte the script. A silly bug fix indeed.

--- qutebrowser/reload.sh
+++ qutebrowser/reload.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+COLORS_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/qutebrowser/noctalia/colors.py"
+
+[ "$COLORS_FILE" -nt "$0" ] || exit 0
+touch "$0"
+
 pgrep -x qutebrowser >/dev/null && qutebrowser :config-source
