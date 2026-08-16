7533958dc453abf305ad5a3804b6c70e29154d92
icy <143240188+bwoah-md@users.noreply.github.com>
2026-08-16 15:58:28 +0530

    Fix pgrep and add comment

--- qutebrowser/reload.sh
+++ qutebrowser/reload.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 COLORS_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/qutebrowser/noctalia/colors.py"
 
+# Skip if colors.py hasn't changed
 [ "$COLORS_FILE" -nt "$0" ] || exit 0
 touch "$0"
 
-pgrep -x qutebrowser >/dev/null && qutebrowser :config-source
+pgrep -f qutebrowser >/dev/null && qutebrowser :config-source
